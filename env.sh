6538f58a3c8beca4a2567e1aa8f3d9bf29db82d4
Viola Lee <31656705+violakyoungalee@users.noreply.github.com>
2018-07-23 17:30:34 -0400

    Delete env.sh

--- env.sh
+++
@@ -1,3 +0,0 @@
-export CLIENT_ID="689387033645-8vom67o8pa2m2q8r7qr1otb5ffufa7q0.apps.googleusercontent.com";
-export CLIENT_SECRET="LkKDCUhUpftvcfRA2YBpRCmi";
-export REDIRECT_URL="http://localhost:1337/oauthcallback";
